3f43b73e568986c01d5f4f4ed690df440c4b889b
Mohit Saini <msaini0r@gmail.com>
2023-02-01 10:07:16 +0530

    todo application

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align="center">Todo React App  📝</h1>
+
+Github Link - <a href="">Link</a>
+
+Netlify Link - <a href="https://chipper-faun-045783.netlify.app">Link</a>
---
+++ build/precache-manifest.dc56df7899f0288c4a77da0ef107fa0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b06a5e2e40dfc5b0cb54b01436606e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14b51d3482798390006d",
+    "url": "/static/css/main.ee5d6995.chunk.css"
+  },
+  {
+    "revision": "04c8f17eb4f56b9b5861",
+    "url": "/static/js/2.b67f85c7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b67f85c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14b51d3482798390006d",
+    "url": "/static/js/main.d41d6d66.chunk.js"
+  },
+  {
+    "revision": "57e0d9b8504c8d95c4b3",
+    "url": "/static/js/runtime-main.5f9abafb.js"
+  }
+]);
